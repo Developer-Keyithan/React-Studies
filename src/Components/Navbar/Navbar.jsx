5923bbcaca6335d65eedf440e5fda0be4c8b83c6
Developer-Keyithan <sathyjaseelankeyithan@gmail.com>
2024-12-23 20:57:23 +0530

    Navbar designed

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,20 @@
+import './Navbar.css'
+
+
+const Navbar = () => {
+  return (
+    <div className='Navbar'>
+        <h1><span className='green'>F</span>shop</h1>
+        <ul>
+            <li>Home</li>
+            <li>About Us</li>
+            <li>Our Services</li>
+            <li>FAQs</li>
+            <li>Contact Us</li>
+        </ul>
+        <a href='https://react-icons.github.io/react-icons/'>Book Us!</a>
+    </div>
+  )
+}
+
+export default Navbar
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import Navbar from './Components/Navbar/Navbar';
 import Name from './Components/Name/Name';
 import Birthday from './Components/Birthday/Birthday';
 import reportWebVitals from './reportWebVitals';
@@ -8,6 +9,7 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Navbar/>
     <Name />
     <Birthday />
   </React.StrictMode>
